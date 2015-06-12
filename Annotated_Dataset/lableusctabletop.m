8b71c2fbbb4f891baf065133b8ba4043206f0aa1
shengshuyang <shengshuyangdlut@gmail.com>
2015-06-12 00:40:59 -0700

    updated script to visualize mask

--- Annotated_Dataset/lableusctabletop.m
+++ Annotated_Dataset/lableusctabletop.m
@@ -1,17 +1,18 @@
-clear all;
-% Define the root folder for the images
-HOMEIMAGES = '.\IGNORE\Images'; % you can set here your default folder
-HOMEANNOTATIONS = '.\IGNORE\Annotations'; % you can set here your default folder
-folderlist = {'users/shengshuyang//usc_table_top'};
-LMinstall (folderlist, HOMEIMAGES, HOMEANNOTATIONS);
-D = LMdatabase(HOMEANNOTATIONS);
-
-% Now you can visualize the images
-LMplot(D, 1, HOMEIMAGES);
+% clear all;
+% % Define the root folder for the images
+% HOMEIMAGES = '.\IGNORE\Images'; % you can set here your default folder
+% HOMEANNOTATIONS = '.\IGNORE\Annotations'; % you can set here your default folder
+% folderlist = {'users/shengshuyang//usc_table_top'};
+% LMinstall (folderlist, HOMEIMAGES, HOMEANNOTATIONS);
+% D = LMdatabase(HOMEANNOTATIONS);
+% 
+% % Now you can visualize the images
+% LMplot(D, 1, HOMEIMAGES);
 
 % Or read an image
-[annotation, img] = LMread(D, 1, HOMEIMAGES);
+[annotation, img] = LMread(D, 25, HOMEIMAGES);
 [mask, class] = LMobjectmask(annotation, HOMEIMAGES);
 image = double(img);
-image(:,:,1) =  image(:,:,1)  + 100*double(mask);
+mask = sum(mask,3);
+image(:,:,1) =  image(:,:,1)  + 50*double(mask);
 imagesc(uint8(image));
